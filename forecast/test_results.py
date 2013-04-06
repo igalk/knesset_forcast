1813676e9ba9bb0d73e0f8bb2f08e93d0237eef7
Nadir Izrael <nadir.izr@gmail.com>
2013-04-06 18:03:54 +0300

    First version of TestResults which writes the CSV.

---
+++ forecast/test_results.py
@@ -0,0 +1,19 @@
+class TestResults:
+
+  def __init__(self):
+    self.rows = [["\"ID\"", "\"Classifier\"", "\"% Training\"",
+                  "\"Use Agendas\"", "\"Use Tags\"", "\"Use Coalition\"",
+                  "\"Use ?\"", "\"% Correct\""]]
+    
+  def addResult(self, member_id, classifier_config, split_percent,
+                ignore_feature_sets, with_wildcards, output):
+    row = ([member_id] + [classifier_config.name] + [split_percent] +
+           [f for f in ignore_feature_sets] + [with_wildcards] +
+           [output.correct_percent])
+    row = [("\"%s\"" % c) for c in row]
+    self.rows.append(row)
+
+  def exportCSV(self, path):
+    csv = file(path, "w")
+    csv.write("".join([(",".join(row) + "\n") for row in self.rows]))
+    csv.close()
