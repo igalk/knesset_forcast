4192fce7454d66c2450ab20bf832c7b5e4c4587b
Nadir Izrael <nadir.izr@gmail.com>
2013-04-06 18:10:44 +0300

    Added "Use BOW"

--- forecast/test_results.py
+++ forecast/test_results.py
@@ -2,7 +2,7 @@ class TestResults:
 
   def __init__(self):
     self.rows = [["\"ID\"", "\"Classifier\"", "\"% Training\"",
-                  "\"Use Agendas\"", "\"Use Tags\"", "\"Use Coalition\"",
+                  "\"Use Agendas\"", "\"Use Tags\"", "\"Use Coalition\"", "\"Use BOW\"",
                   "\"Use ?\"", "\"% Correct\""]]
     
   def addResult(self, member_id, classifier_config, split_percent,
@@ -11,6 +11,7 @@ class TestResults:
            [f for f in ignore_feature_sets] + [with_wildcards] +
            [output.correct_percent])
     row = [("\"%s\"" % c) for c in row]
+    assert len(row) == len(self.rows[0])
     self.rows.append(row)
 
   def exportCSV(self, path):
